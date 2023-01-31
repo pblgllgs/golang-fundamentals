072e1f8a73423a159a3618b649ef50377c4ae858
pblgllgs <pbl.gllgs@gmail.com>
2023-01-31 01:38:11 -0300

    arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,48 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func lastProduct(arr []Product) int {
+	return count(arr) - 1
+}
+
+func count(arr []Product) int {
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i].name != "" {
+			sum += 1
+		}
+	}
+	return sum
+}
+
+func totalCost(arr []Product) int {
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i].name != "" {
+			sum += arr[i].price
+		}
+	}
+	return sum
+}
+
+func info(arr []Product) {
+	fmt.Println("the last product is", arr[lastProduct(arr[:])].name)
+	fmt.Println("the total of products is", count(arr[:]))
+	fmt.Println("the total cost of products is", totalCost(arr[:]))
+}
+
+func main() {
+	products := [4]Product{
+		{"Bnana", 1},
+		{"Meat", 6},
+		{"Salad", 3},
+	}
+	info(products[:])
+	products[3] = Product{"Brad", 4}
+	info(products[:])
 }
