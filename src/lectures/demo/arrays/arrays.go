0c1602140e838c538acbead313034ed4d62999ca
pblgllgs <pbl.gllgs@gmail.com>
2023-01-31 01:38:42 -0300

    arrays

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanLiness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean!!")
+		} else {
+			fmt.Println(room.name, "is dirty!!")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "one", cleaned: true},
+		{name: "two", cleaned: false},
+		{name: "tree", cleaned: false},
+		{name: "four", cleaned: true},
+	}
+	checkCleanLiness(rooms)
 }
