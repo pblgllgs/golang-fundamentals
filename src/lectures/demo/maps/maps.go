1f886a5ef90280c0d22081c19138577ddf205036
pblgllgs <pbl.gllgs@gmail.com>
2023-02-01 23:17:31 -0300

    ranges and maps

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,33 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 1
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+
+	fmt.Println("Milk deleted, ner list: ", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	fmt.Println("Need cereal?")
+	if !found {
+		fmt.Println("not cereal")
+	} else {
+		fmt.Println("yes!", cereal, "boxes")
+	}
+
+	totalCount := 0
+	for _, amount := range shoppingList {
+		totalCount += amount
+	}
+
+	fmt.Println("count", totalCount)
 }
--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -26,6 +26,48 @@ const (
 	Retired     = 3
 )
 
+func info(list map[string]int) {
+	countOnline := 0
+	countOffline := 0
+	countMaintenance := 0
+	countRetired := 0
+	for key, value := range list {
+		switch value {
+		case 0:
+			fmt.Println("The status of", key, "is online")
+			countOnline += 1
+		case 1:
+			fmt.Println("The status of", key, "is offline")
+			countOffline += 1
+		case 2:
+			fmt.Println("The status of", key, "is maintance")
+			countMaintenance += 1
+		case 3:
+			fmt.Println("The status of", key, "is retired")
+			countRetired += 1
+		}
+	}
+	fmt.Println("The count servers online is", countOnline)
+	fmt.Println("The count servers offline is", countOffline)
+	fmt.Println("The count servers in maintenance is", countMaintenance)
+	fmt.Println("The count servers retired is", countRetired)
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+	serverStatus := make(map[string]int)
+	for _, server := range servers {
+		serverStatus[server] = Online
+	}
+	fmt.Println("--------Default Status--------")
+	info(serverStatus)
+	serverStatus["darkstar"] = 3
+	serverStatus["aiur"] = 1
+	fmt.Println("--------Change some Status--------")
+	info(serverStatus)
+	for _, server := range servers {
+		serverStatus[server] = Maintenance
+	}
+	fmt.Println("--------Final Status--------")
+	info(serverStatus)
 }
