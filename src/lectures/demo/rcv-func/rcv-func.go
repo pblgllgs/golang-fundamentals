2a6805d5cbaf1abc514e5ef0bba6f91f1cfe1471
pblgllgs <pbl.gllgs@gmail.com>
2023-02-06 22:37:55 -0300

    receiver functions

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+type Space struct {
+	occupied bool
+}
+
+type ParkingSlot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingSlot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingSlot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingSlot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
 func main() {
 
+	lot := ParkingSlot{spaces: make([]Space, 4)}
+	fmt.Println(lot)
+	lot.occupySpace(1)
+
+	occupySpace(&lot, 2)
+	fmt.Println(lot)
+
+	lot.vacateSpace(2)
+	fmt.Println(lot)
 }
--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,67 @@ package main
 
 import "fmt"
 
+type Player struct {
+	health    int
+	maxHealth int
+	energy    int
+	maxEnergy int
+	name      string
+}
+
+const (
+	MAX_ENERGY = 500
+	MAX_HEALTH = 100
+)
+
+func (player *Player) addHealth(health int) {
+	if health+player.health > MAX_HEALTH {
+		player.health = MAX_HEALTH
+	} else {
+		player.health += health
+		fmt.Println("add", health, "of health ->", "the new health is:", player.health)
+	}
+
+}
+
+func (player *Player) consumeHealth(health int) {
+	if health > player.health {
+		player.health = 0
+		fmt.Println("The damage is", health, "->", "the new health is:", player.health)
+	} else {
+		player.health -= health
+		fmt.Println("The damage is", health, "->", "the new health is:", player.health)
+	}
+}
+
+func (player *Player) addEnergy(energy int) {
+	if energy+player.energy > MAX_ENERGY {
+		player.energy = MAX_ENERGY
+	} else {
+		player.energy += energy
+		fmt.Println("add", energy, "of energy ->", "the new energy is:", player.energy)
+	}
+}
+
+func (player *Player) consumeEnergy(energy int) {
+	if energy > player.energy {
+		player.health = 0
+	} else {
+		player.energy -= energy
+		fmt.Println("The consume is", energy, "->", "the new energy is:", player.energy)
+	}
+}
+
 func main() {
 
+	player1 := Player{name: "MAX", energy: 500, maxEnergy: 500, health: 100, maxHealth: 100}
+	player1.consumeHealth(99)
+	// player1.info()
+	player1.addHealth(10)
+	// player1.info()
+	player1.consumeEnergy(20)
+	// player1.info()
+	player1.addEnergy(10)
+	// player1.info()
+	player1.consumeHealth(9999)
 }
